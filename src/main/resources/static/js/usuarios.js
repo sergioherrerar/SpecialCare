fb41e2c59f023c3383689ac7e48562df00627fa0
impacevanend <54188811+impacevanend@users.noreply.github.com>
2023-08-19 20:28:24 -0500

    Se agregan nuevas funcionalidades al código

--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -1,6 +1,6 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-    cagarUsuarios();
+  cagarUsuarios();
   $('#usuarios').DataTable();
 });
 
@@ -13,16 +13,21 @@ async function cagarUsuarios() {
       'Content-Type': 'application/json'
     }
   });
-
   const usuarios = await request.json();
-  let listadoHtml = "";
-  for (let usuario of usuarios){
-    let usuarioHtml = '<tr><td>'+usuario.id+'</td><td>'+usuario.Name+' '+usuario.Lastname+'</td><td>'
-            + usuario.email+'</td><td>'+usuario.Phone_Number
-            + '</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a><a href="#" class="btn btn-warning btn-circle btn-sm"><i class="fas fa-exclamation-triangle"></i></a><a href="#" class="btn btn-info btn-circle btn-sm"><i class="fas fa-info-circle"></i></a></td></tr>';
+  let listadoHtml = ''
+  for (let usuario of usuarios) {
 
-    listadoHtml += usuarioHtml;
+
+    let usuarioHtml = `<tr><td>${usuario.id}</td><td>${usuario.name + " " + usuario.lastname}</td>
+                       <td>${usuario.email}</td><td>${usuario.password}</td><td>${usuario.phone_Number}</td>
+                       <td><a href="#" class="btn btn-danger btclassNamecle btn-sm"><i class="fas fa-trash"></iclassName </a></td></tr>`
+
+    listadoHtml += usuarioHtml
   }
-document.querySelector("#usuarios tbody").outerHTML="listadoHtml";
+
+ //console.log(usuarios);
+
+
+ document.querySelector('#usuarios tbody').outerHTML = listadoHtml;
 
 }
