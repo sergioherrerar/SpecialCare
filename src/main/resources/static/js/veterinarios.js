8221e7c29266a536aa4ebf1ec5b9ed5bfb83c0c1
impacevanend <54188811+impacevanend@users.noreply.github.com>
2023-08-22 16:20:02 -0500

    Se agregan botonoes a nivel de veterinario

--- src/main/resources/static/js/veterinarios.js
+++ src/main/resources/static/js/veterinarios.js
@@ -20,10 +20,19 @@ async function cagarVeterinarios() {
 
     let listadoHtml = '';
     for (let veterinario of veterinarios) {
+        let getButton =
+            '<a href="#" onclick="getRecord(' +
+            veterinario.id +
+            ')" class="btn btn-primary btn-circle btn-sm"><i class="fas fa-eye"></i></a>';
+
+        let updateButton = '<a href="#" onclick="updateRecord(' + veterinario.id +
+            ')" class="btn btn-warning btn-circle btn-sm"><i class="fas fa-pencil"></i></a>';
+
         let botonEliminar = '<a href="#" onclick="eliminarUsuario(' + veterinario.id + ')" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a>';
         console.log("lo que obtienes: "+veterinarios[0].veterinarianSpeciality.name)
-        let veterinarioHtml = '<tr><td>'+veterinario.id+'</td><td>' + veterinarios[0].veterinarianSpeciality.name  + '</td><td>'
-            + veterinario.fullname+'</td><td>' + botonEliminar + '</td></tr>';
+
+        let veterinarioHtml =`<tr><td>${veterinario.id}</td><td> ${veterinarios[0].veterinarianSpeciality.name}  </td><td>
+            ${veterinario.fullname}</td><td> ${getButton}  ${updateButton} ${botonEliminar}</td></tr>` ;
         listadoHtml += veterinarioHtml;
     }
 
