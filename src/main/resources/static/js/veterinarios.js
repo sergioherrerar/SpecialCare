f09f004b36d4e34f2f1108ab6d382de8b8f9cfd8
impacevanend <54188811+impacevanend@users.noreply.github.com>
2023-08-22 15:36:26 -0500

    se envia front y lógica de vista veterinario

---
+++ src/main/resources/static/js/veterinarios.js
@@ -0,0 +1,55 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+    cagarVeterinarios();
+    $('#veterinario').DataTable();
+});
+
+async function cagarVeterinarios() {
+
+    const request = await fetch('api/veterinarians', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const veterinarios = await request.json();
+    //console.log(veterinarios[0].veterinarianSpeciality.name);
+
+
+    let listadoHtml = '';
+    for (let veterinario of veterinarios) {
+        let botonEliminar = '<a href="#" onclick="eliminarUsuario(' + veterinario.id + ')" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a>';
+        console.log("lo que obtienes: "+veterinarios[0].veterinarianSpeciality.name)
+        let veterinarioHtml = '<tr><td>'+veterinario.id+'</td><td>' + veterinarios[0].veterinarianSpeciality.name  + '</td><td>'
+            + veterinario.fullname+'</td><td>' + botonEliminar + '</td></tr>';
+        listadoHtml += veterinarioHtml;
+    }
+
+    document.querySelector("#veterinario tbody").outerHTML=listadoHtml;
+
+}
+
+
+async function eliminarUsuario(id){
+
+    if(!confirm(`¿Desea eliminar este usuario`)){
+        return;
+    }
+
+    function getHeaders() {
+        return {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.token
+        };
+    }
+
+
+    const request = await fetch('api/usuarios/' + id, {
+        method: 'DELETE',
+        headers: getHeaders()
+    });
+    location.reload();
+}
